60e147f13f610e69792439a85daf67bd8c5c7391
Himanshu <himanshucosmicera@gmail.com>
2023-04-24 21:09:12 +0530

    created all container structure for web app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import Header from './container/Header/Header';
+import About from './container/About/About';
+import Globe from './container/Globe/Globe';
+import Explore from './container/Explore/Explore';
+import Blogs from './container/Blogs/Blogs';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Header />
+      <About />
+      <Globe />
+      <Explore />
+      <Blogs />
     </div>
   );
 }
