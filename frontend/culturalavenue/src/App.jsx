e5022fe64cbb652a0d774b640b2e35da8d700d85
gyan2003 <gyanendarsingh891@gmail.com>
2023-04-24 13:04:25 +0530

    first noob commit

---
+++ frontend/culturalavenue/src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./App.css";
+import Navbar from "../src/Components/Navbar/Navbar";
+const App = () => {
+  return (
+    <div>
+       
+      <Navbar />
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis inventore nisi odit vitae dolor non iure, impedit placeat cum, expedita recusandae. Numquam voluptate nam molestiae beatae consequatur. Beatae, fugiat?
+       
+
+    </div>
+  );
+};
+
+export default App;
