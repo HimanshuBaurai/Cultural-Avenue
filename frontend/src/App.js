9d7275bb8fab5589eae37e12e14d47c322ab3cfe
Himanshu <himanshucosmicera@gmail.com>
2023-08-09 01:20:58 +0530

    added extension name .jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.scss';
-import Navbar from './components/Navbar/Navbar';
-import Header from './container/Header/Header';
-import About from './container/About/About';
-import Globe from './container/Globe/Globe';
-import Explore from './container/Explore/Explore';
-import Blogs from './container/Blogs/Blogs';
-import Footer from './container/Footer/Footer';
+import Navbar from './components/Navbar/Navbar.jsx';
+import Header from './container/Header/Header.jsx';
+import About from './container/About/About.jsx';
+import Globe from './container/Globe/Globe.jsx';
+import Explore from './container/Explore/Explore.jsx';
+import Blogs from './container/Blogs/Blogs.jsx';
+import Footer from './container/Footer/Footer.js';
 
 function App() {
   return (
